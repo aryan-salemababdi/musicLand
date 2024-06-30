5d9e48c1de7462cb766a69cd3c56f93294c32ab5
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-06-30 15:22:39 +0330

    change theme

--- src/components/Atom/Header/Header.tsx
+++ src/components/Atom/Header/Header.tsx
@@ -40,7 +40,7 @@ const Header: NextPage<HeaderProps> = ({
         `
           h-fit 
           bg-gradient-to-b
-          from-emerald-800
+          from-red-800
           p-6
       `,
         className
