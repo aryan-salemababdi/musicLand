8f94d5d97c16197297c9ac511e130d2dc5dffc29
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-07-08 22:48:05 +0330

    some cheange

--- src/components/Atom/SongItem/SongItem.tsx
+++ src/components/Atom/SongItem/SongItem.tsx
@@ -22,7 +22,6 @@ const SongItem: NextPage<SongItemProps> = ({
 
     return (
         <div
-            onClick={() => onClick(data.id)}
             className="
         relative 
         group
@@ -39,8 +38,9 @@ const SongItem: NextPage<SongItemProps> = ({
         p-3
         "
         >
-            <div
-                className="
+            <div onClick={() => onClick(data.id)}>
+                <div
+                    className="
             relative 
             aspect-square
             w-full
@@ -48,21 +48,22 @@ const SongItem: NextPage<SongItemProps> = ({
             rounded-md
             overflow-hidden
             "
-            >
-                <Image
-                    loading="eager"
-                    className="object-cover"
-                    src={`https://zxfaokwchdpwczfkmsen.supabase.co/storage/v1/object/public/images/${data.image_path}` || '/images/liked.png'}
-                    fill
-                    alt="Image"
-                />
-            </div>
-            <div className="flex flex-col items-start w-full pt-4 gap-y-1">
-                <p className="font-semibold text-xl truncate w-full">{data.title}</p>
-                <p className="text-neutral-400 text-sm pb-4 w-full truncate">By {data.author}</p>
-            </div>
-            <div className="absolute bottom-24 right-5">
-                <PlayButton />
+                >
+                    <Image
+                        loading="eager"
+                        className="object-cover"
+                        src={`https://zxfaokwchdpwczfkmsen.supabase.co/storage/v1/object/public/images/${data.image_path}` || '/images/liked.png'}
+                        fill
+                        alt="Image"
+                    />
+                </div>
+                <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+                    <p className="font-semibold text-xl truncate w-full">{data.title}</p>
+                    <p className="text-neutral-400 text-sm pb-4 w-full truncate">By {data.author}</p>
+                </div>
+                <div className="absolute bottom-24 right-5">
+                    <PlayButton />
+                </div>
             </div>
             {activeSongId === data.id && (
                 <audio
